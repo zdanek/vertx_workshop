19608196fa91e5b6b1726e6c1aef0b42a1946172
Bartek Zdanowski <bzd@touk.pl>
2015-10-10 01:31:19 +0200

    session and global counter

--- src/main/java/io/vertx/workshop/web/RoutingWebServer.java
+++ src/main/java/io/vertx/workshop/web/RoutingWebServer.java
@@ -3,11 +3,16 @@ package io.vertx.workshop.web;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CookieHandler;
 import io.vertx.ext.web.handler.FaviconHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
+
+import java.util.Optional;
 
 /**
  * Created by bartek on 09.10.15.
@@ -34,8 +39,29 @@ public class RoutingWebServer extends AbstractVerticle {
         router.route("/eventbus/*").handler(sockJSHandler);
 
         router.route().handler(FaviconHandler.create(ROOT + "/favicon.ico"));
-        router.route().handler(StaticHandler.create(ROOT));
+        //SessionHandler must be preceded with SessionHandler
+        router.route().handler(CookieHandler.create());
+        router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
 
+        router.get("/sessionCounter").handler(ctx -> {
+            int counter = (int) Optional.ofNullable(ctx.session().get("counter")).orElse(0);
+            counter++;
+            ctx.session().put("counter", counter);
+
+            ctx.response().end(String.valueOf(counter));
+        });
+
+        router.get("/globalCounter").handler(ctx -> {
+            int counter = (int) Optional.ofNullable(vertx.sharedData().getLocalMap("data").get("counter")).orElse(0);
+            counter++;
+            vertx.sharedData().getLocalMap("data").put("counter", counter);
+
+            ctx.response().end(String.valueOf(counter));
+        });
+
+
+        //last!
+        router.route().handler(StaticHandler.create(ROOT));
         server.requestHandler(router::accept).listen(8080);
 
     }
